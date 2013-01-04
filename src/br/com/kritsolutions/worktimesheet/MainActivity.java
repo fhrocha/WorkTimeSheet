30a5eeaf97e348cc6304fe4a34c54ac717841379
Fábio Henrique da Rocha <fhrocha@kr-itsolutions.com.br>
2013-01-04 11:48:45 -0200

    Calculate distance

--- src/br/com/kritsolutions/worktimesheet/MainActivity.java
+++ src/br/com/kritsolutions/worktimesheet/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.kritsolutions.worktimesheet;
 
+import java.math.BigDecimal;
+
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
@@ -13,8 +15,11 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
+	private static final int INTERVAL_MINUTES = 1000 * 60 * 1;
+	private static final float THIRTY_METERS = 30;
 	private LocationManager locationManager;
-	private Button position;
+	private Double initialUserLatitude;
+	private Double initialUserLongitude;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +45,12 @@ public class MainActivity extends Activity {
 		location = location == null ? getNETWORKLocation() : location;
 		
 		if( location != null ) {
+			
+			initialUserLatitude = location.getLatitude();
+			initialUserLongitude = location.getLongitude();
+			
 			message = String.format("Current location \n Longitude: %1$s \n Latitude: %2$s", 
-					location.getLongitude(), location.getLatitude());
+					initialUserLongitude, initialUserLatitude);
 			
 			Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
 		}		
@@ -49,13 +58,19 @@ public class MainActivity extends Activity {
 
 	private Location getGPSLocation() {
 		
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1, new MyLocationListener());		
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 
+												INTERVAL_MINUTES, 
+												THIRTY_METERS , 
+												new MyLocationListener());		
 		return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 	}
 
 	private Location getNETWORKLocation() {
 
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 1, new MyLocationListener());
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 
+												INTERVAL_MINUTES, 
+												THIRTY_METERS, 
+												new MyLocationListener());
 		return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 	}
 	
@@ -63,9 +78,13 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onLocationChanged(Location _location) {
+			
+			double longitude = _location.getLongitude();
+			double latitude = _location.getLatitude();
+			Double distance = calculateDistance(initialUserLatitude, initialUserLongitude, latitude, longitude);
 
-			String message = String.format("New location \n Longitude: %1$s \n Latitude: %2$s", 
-					_location.getLongitude(), _location.getLatitude());
+			String message = String.format("New location \n Longitude: %1$s \n Latitude: %2$s \n Distance: %3$s", 
+					longitude, latitude, distance);
 			
 			Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
 		}
@@ -88,6 +107,25 @@ public class MainActivity extends Activity {
 			
 		}
 		
+	}
+	
+	private static Double calculateDistance(Double _latitudeOri, Double _longitudeOri, Double _latitudeDes, Double _longitudeDes) {
+		
+		Float EARTH_RADIUS_KM = 6371f;
+		
+		Double dLat = Math.toRadians( _latitudeDes - _latitudeOri );
+		Double dLon = Math.toRadians( _longitudeDes - _longitudeOri );
+		
+		Double latOriRadius = Math.toRadians( _latitudeOri );
+		Double latDesRadius = Math.toRadians( _latitudeDes );
+		
+		Double haversine_a = Math.sin( dLat / 2 ) * Math.sin( dLat / 2 ) 
+							+ Math.sin( dLon / 2 ) * Math.sin( dLon / 2 )
+							* Math.cos( latOriRadius ) * Math.cos( latDesRadius );
+		
+		Double haversine_c = 2 * Math.atan2( Math.sqrt( haversine_a ), Math.sqrt( 1 - haversine_a ) );
+		
+		return new BigDecimal(EARTH_RADIUS_KM * haversine_c).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
 	}
 
 }
